117ba4b0078db9c54b5d17693f14771784d748b3
Thomas <thomas@ping-it.be>
2017-08-04 12:24:01 +0200

    deleted iTop files

---
+++ intra/web/env-production/dictionaries/languages.php
@@ -0,0 +1,78 @@
+<?php
+//
+// Dictionary index built by the compiler
+//
+Dict::SetLanguagesList(
+array (
+  'DA DA' => 
+  array (
+    'description' => 'Danish',
+    'localized_description' => 'Dansk',
+  ),
+  'FR FR' => 
+  array (
+    'description' => 'French',
+    'localized_description' => 'Français',
+  ),
+  'PT BR' => 
+  array (
+    'description' => 'Brazilian',
+    'localized_description' => 'Brazilian',
+  ),
+  'RU RU' => 
+  array (
+    'description' => 'Russian',
+    'localized_description' => 'Русский',
+  ),
+  'JA JP' => 
+  array (
+    'description' => 'Japanese',
+    'localized_description' => '日本語',
+  ),
+  'DE DE' => 
+  array (
+    'description' => 'German',
+    'localized_description' => 'Deutsch',
+  ),
+  'HU HU' => 
+  array (
+    'description' => 'Hungarian',
+    'localized_description' => 'Magyar',
+  ),
+  'CS CZ' => 
+  array (
+    'description' => 'Czech',
+    'localized_description' => 'Čeština',
+  ),
+  'ES CR' => 
+  array (
+    'description' => 'Spanish',
+    'localized_description' => 'Español, Castellano',
+  ),
+  'IT IT' => 
+  array (
+    'description' => 'Italian',
+    'localized_description' => 'Italiano',
+  ),
+  'EN US' => 
+  array (
+    'description' => 'English',
+    'localized_description' => 'English',
+  ),
+  'NL NL' => 
+  array (
+    'description' => 'Dutch',
+    'localized_description' => 'Nederlands',
+  ),
+  'ZH CN' => 
+  array (
+    'description' => 'Chinese',
+    'localized_description' => '简体中文',
+  ),
+  'TR TR' => 
+  array (
+    'description' => 'Turkish',
+    'localized_description' => 'Türkçe',
+  ),
+)
+);
